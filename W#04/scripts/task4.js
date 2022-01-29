9cd3eee96e671a02474b6754cee6566684a06b42
albertoalmada <albertoalmada@yahoo.com>
2022-01-28 22:09:45 -0300

    final

--- W#04/scripts/task4.js
+++ W#04/scripts/task4.js
@@ -3,6 +3,21 @@
 /* DATA */
 
 // Step 1: Declare a new variable to hold information about yourself
+let data = {
+    name: "Alberto Parreira Almada",
+    photo: "images/albertoalmada.png",
+    favoriteFoods: [
+        'lasagna',
+        'fish',
+        'barbecue',
+        'icecream'
+    ],
+    hobbies: [
+        'TV series',
+        'driving',
+        'trips'
+    ],
+}
 
 // Step 2: Inside of the object, add a property named name with a value of your name as a string
 
@@ -13,8 +28,14 @@
 // Step 5: Add another property named hobbies with a value of an array of your hobbies as strings
 
 // Step 6: Add another property named placesLived with a value of an empty array
-
-// Step 7: Inside of the empty array above, add a new object with two properties: place and length and values of an empty string
+data.placesLived = [{
+        place: 'São Paulo, Brazil',
+        duration: '39 years'
+    }, {
+        place: 'São Bernardo do Campo, Brazil',
+        duration: '4 years'
+    }]
+    // Step 7: Inside of the empty array above, add a new object with two properties: place and length and values of an empty string
 
 // Step 8: For each property, add appropriate values as strings
 
@@ -24,21 +45,42 @@
 /* OUTPUT */
 
 // Step 1: Assign the value of the name property (of the object declared above) to the HTML <span> element with an ID of name
+document.querySelector('#name').textContent = data.name;
 
 // Step 2: Assign the value of the photo property as the src attribute of the HTML <img> element with an ID of photo
+document.querySelector('#photo').setAttribute('src', data.photo);
 
 // Step 3: Assign the value of the name property as the alt attribute of the HTML <img> element with an ID of photo
+document.querySelector('#photo').setAttribute('alt', data.name);
 
 // Step 4: For each favorite food in the favoriteFoods property, create an HTML <li> element and place its value in the <li> element
+data.favoriteFoods.forEach((food) => {
+    let favoriteFoodElem = document.createElement('li');
+    favoriteFoodElem.textContent = food;
 
-// Step 5: Append the <li> elements created above as children of the HTML <ul> element with an ID of favorite-foods
-
+    // Step 5: Append the <li> elements created above as children of the HTML <ul> element with an ID of favorite-foods
+    document.querySelector('#favorite-foods').appendChild(favoriteFoodElem);
+});
 // Step 6: Repeat Step 4 for each hobby in the hobbies property
+data.hobbies.forEach((hobbies) => {
+    let hobbiesElem = document.createElement('li');
+    hobbiesElem.textContent = hobbies;
+    document.querySelector('#hobbies').appendChild(hobbiesElem);
+});
 
 // Step 7: Repeat Step 5 using the HTML <ul> element with an ID of hobbies
 
 // Step 8: For each object in the <em>placesLived</em> property:
 // - Create an HTML <dt> element and put its place property in the <dt> element
 // - Create an HTML <dd> element and put its length property in the <dd> element
+data.placesLived.forEach((placeLived) => {
+    let placesLivedCity = document.createElement('dt');
+    placesLivedCity.textContent = placeLived.place;
 
-// Step 9: Append the HTML <dt> and <dd> elements created above to the HTML <dl> element with an ID of places-lived
+    let placesLivedDuration = document.createElement('dd');
+    placesLivedDuration.textContent = placeLived.duration;
+
+    // Step 9: Append the HTML <dt> and <dd> elements created above to the HTML <dl> element with an ID of places-liveddocument.querySelector('#places-lived').appendChild(placesLivedPlace);
+    document.querySelector('#places-lived').appendChild(placesLivedCity);
+    document.querySelector('#places-lived').appendChild(placesLivedDuration);
+})
